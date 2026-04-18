8e3af5a7407f2b721e55e092e234f9f8cb300b41
Royality <skawns521@gmail.com>
2026-04-18 12:59:28 +0900

    1. Hello Spring Boot Application 2. /hello, /login 기능 구현 및 테스트

---
+++ src/main/java/com/example/hello/controler/HelloControler.java
@@ -0,0 +1,44 @@
+package com.example.hello.controler;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Request:
+ * Post /api/user/ http 1.1
+ * Host: api.example.com
+ * Content-type: Application/Json
+ *
+ * {
+ *     name=어쩌구
+ * }
+ *
+ * Response:
+ * Http/ 1.1 200 OK
+ * Content-type: Application/Json
+ *
+ * {
+ *     json data...
+ * }
+ * */
+
+@RestController
+public class HelloControler {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "World")String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/login")
+    public String login(String id, String pw) {
+
+        if (id.equals("skawns521") && pw.equals("1234")) {
+            return "로그인 성공";
+        }
+        return "로그인 실패";
+    }
+}
